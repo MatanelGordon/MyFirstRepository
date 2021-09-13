a3f764cc43ede08a2e833cf373582fa19885de74
Mat Gordon <mmgordon.matanel@gmail.com>
2021-09-13 14:39:25 +0300

    buidling the boilerplate

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+function App() {
+    return (
+        <div>
+            HELLO THERE
+        </div>
+    )
+}
+
+render(<App></App>, document.querySelector("#root"))
